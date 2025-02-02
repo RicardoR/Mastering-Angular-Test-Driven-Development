8b5159a35434ab9404d28769ff8683e0aa418be7
Ricardo <ricardo.rv@outlook.com>
2025-02-02 20:31:09 +0100

    test: should add two numbers correctly

--- Chapter 2/getting-started-angular-tdd/src/app/calculator/calculator.component.spec.ts
+++ Chapter 2/getting-started-angular-tdd/src/app/calculator/calculator.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
+import {CalculatorService} from "../../core/services/calculator.service";
 
 describe('CalculatorComponent', () => {
   let calculator: CalculatorComponent;
@@ -32,5 +33,12 @@ describe('CalculatorComponent', () => {
   it('should initialize result to 0', () => {
     calculator.ngOnInit();
     expect(calculator.result).toBe(0);
+  });
+
+  it('should add two numbers correctly', () => {
+    spyOn(calculatorService, 'add').and.callThrough();
+    calculator.add(2,3);
+    expect(calculatorService.add).toHaveBeenCalledWith(2,3);
+    expect(calculator.result).toBe(5);
   });
 });
